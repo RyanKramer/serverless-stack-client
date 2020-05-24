89ba21f33cc68f60f6aa2a8b06a64e2ed45890df
Ryan Kramer <ryantheba@gmail.com>
2020-05-24 13:31:49 -0400

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "the-notes-app-uploads123"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://dejhdt6in6.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_3UDkvpk3H",
+        APP_CLIENT_ID: "6v9k567qntdu7bgmndjokughvp",
+        IDENTITY_POOL_ID: "us-east-2:5e41a93c-35d2-47f5-ab73-bdfb5e363aa1"
+    }
+};
